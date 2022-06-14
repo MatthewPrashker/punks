2fcf38402513c81edf4b43eedf5e1636e1c0a3ab
MatthewPrashker <prashker.m@northeastern.edu>
2022-06-13 20:47:05 -0400

    punks in vault

--- test/foundry/OnChain.t.sol
+++ test/foundry/OnChain.t.sol
@@ -6,21 +6,31 @@ import "forge-std/console2.sol";
 
 import "solmate/tokens/ERC20.sol";
 import "solmate/tokens/ERC721.sol";
+import "solmate/tokens/ERC1155.sol";
 
 import "../../contracts/Example.sol";
 
 contract TestExample is Test {
-    address internal immutable punkAddr =
-        0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB;
+    address internal immutable nftAddr =
+        0x2fC722C1c77170A61F17962CC4D039692f033b43; //Milady
+
+    address internal immutable punkVTokenAddr =
+        0x269616D549D7e8Eaa82DFb17028d0B212D11232A;
 
     function setUp() public payable {}
 
-    function testPunks() public {
-        console.log(punkAddr);
-        ERC721 punk = ERC721(punkAddr);
-        console.log(punk.name());
-        //for(uint256 i = 0; i < 4; i++) {
-        //  console.log(punk.tokenURI(i));
-        //}
+    function testNFTName() public {
+        console.log("Examing NFT at", nftAddr);
+        ERC721 nft = ERC721(nftAddr);
+        console.log(nft.name());
+    }
+
+    function testVToken() public {
+        ERC20 punkVToken = ERC20(punkVTokenAddr);
+        //There will be exactly one punk in the nftx token vault for every vToken in existence.
+        console.log(
+            "Number of punks inside the vault at this time",
+            punkVToken.totalSupply() / (10**18)
+        );
     }
 }
